1c2e065dc03bfe030ae9026a42005c9a05759d10
IbrahimElFitiany <ibrahimelfitiany@gmail.com>
2025-08-15 03:09:36 +0300

    feat(manager-store): add zustand manager-store

---
+++ apps/sufra-restaurant/src/stores/authStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import type { Manager } from "@/types/Manager";
+
+type ManagerState = {
+  manager: Manager;
+  setManager: (manager: Manager) => void;
+  clearManager: () => void;
+};
+
+export const useManagerStore = create<ManagerState>()(
+  persist( (set, get, api) => ({manager: {
+        managerID: 0,
+        fname: "",
+        lname: "",
+        email: "",
+        restaurantId: 0,
+        restaurantName: "",
+      },
+      setManager: (data) => set({ manager: data }),
+      clearManager: () =>{
+        set({
+          manager: {
+            managerID: 0,
+            fname: "",
+            lname: "",
+            email: "",
+            restaurantId: 0,
+            restaurantName: "",
+          },
+        })
+        api.persist.clearStorage(); 
+      },
+    }),
+    {
+      name: "manager-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
